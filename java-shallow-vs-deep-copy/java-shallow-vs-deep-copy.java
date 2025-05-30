dd24870f7623d4eb6ad71679f18ba449d2ac388f
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-05-30 11:39:59 +0530

    Create java-shallow-vs-deep-copy.java

---
+++ java-shallow-vs-deep-copy/java-shallow-vs-deep-copy.java
@@ -0,0 +1,113 @@
+/**
+The Cloneable Interface: its a marker interface, this means it doesn't contains any methods but serves as a signnal to Java runtime that the class
+allows for field-for-field copying of instances. When a class implements **Clonable**, it indicates that it supports the creation of a copy through 
+the clone() method.
+*/
+
+/**
+Implementing Clonning:
+1. Implement the Cloneable interface in your class.
+2. Override the clone() method from the Object class.
+3. Call super.clone() inside the clone() method.
+*/
+
+class Person implements Cloneable{
+  String name;
+  int age;
+  Person(String name, int age){
+    this.name=name;
+    this.age=age;
+  }
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
+  @Override
+  public String toString(){
+    return "Person{name='" +name+"', age="+age+"}";
+  }
+  public static void main(String[] args){
+    try{
+      Person original=new Person("Alice", 30);
+      Person cloned=(Person)original.clone();
+      System.out.println("Original: "+ original);
+      System.out.println("Cloned: "+ cloned);
+    }catch(CloneNotSupportedException e){
+      e.printStackTrace();
+    }
+  }
+}
+
+
+
+////// Shallow Copy:
+/**
+A shallow copy of an object copies all the member fields, but it does not copy objects that the fields refer to.
+Instead, the references to these objects are copied, meaning both the original and copied object refer to the same objects.
+
+In Java, the clone() method creates a shallow copy by default.
+*/
+
+
+// ### Example of Shallow Copy:
+class Address{
+  String city;
+  Address(String city){
+    this.city=city;
+  }
+}
+
+class Person implements Cloneable{
+  String name;
+  int age;
+  Address address;
+  
+  Person (String name, int age, Address address){
+    this.name=name;
+    this.age=age;
+    this.address=address;
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException{
+    return super.clone();
+  }
+  @Override
+  public String toString(){
+    return "Person{name='"+name+"', age="+age+", address="+address.city+"}";
+  }
+
+  public static void main(String[] args){
+    try{
+      Address address=new Address("New York");
+      Person original=new Person("Alice", 30, address);
+      Person cloned=(Person) original.clone();
+      System.out.println("Original: "+original);
+      System.out.println("Cloned: "+cloned);
+      //modify the address 
+      cloned.address.city="Los angeles";
+      System.out.println("After Modifying cloned object:");
+      System.out.println("Original: "+original);
+      System.out.println("Cloned: "+cloned);
+    }catch(CloneNotSupportedException e){
+      e.printStackTrace();
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
