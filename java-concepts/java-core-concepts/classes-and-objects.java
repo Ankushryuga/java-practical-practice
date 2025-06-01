c45c696257a5323757824324888dc474cc64d903
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-06-01 10:23:49 +0530

    Create classes-and-objects.java

---
+++ java-concepts/java-core-concepts/classes-and-objects.java
@@ -0,0 +1,14 @@
+/**
+Java is a class-mandatory programming language that imposes an object model on the developer.
+Class serve as a prototype for representing objects that group pieces of data and methods. An object is an entity
+that has a state and behaviour.
+
+
+## Properties of Java Classes
+1. A Java class serves as a blueprint for creating objects and doesn't take up memory.
+2. It comprises variables of various types and methods. You can include data members, methods, constructors, nested classes, and interfaces within a class.
+3. It acts as a template for organizing and defining the behaviour of objects in your program.
+
+## Syntax of Java Classes
+One can declare a public, private, or default class as:
+*/
