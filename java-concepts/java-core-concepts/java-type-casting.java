86138e1238192c14d44d7854e1b2ef3f194d2323
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-06-01 14:21:33 +0530

    Create java-type-casting.java

---
+++ java-concepts/java-core-concepts/java-type-casting.java
@@ -0,0 +1,11 @@
+/**
+Type casting in java is the process of converting one data type to another, it can be done automatically or manually.
+Automatic type casting occurs when a value of one data type is assigned to another compatible data type.
+
+
+# There are 2 types of casting: widening type casting and narrowing type casting.
+TypeCasting: 
+Type means the data type of a variable or entity and Casting means converting the data type of an entity to another data type.
+
+
+*/
